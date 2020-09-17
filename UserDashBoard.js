809d674c177411e8647b1617c764f65edb3fecc8
girishjulka <68934766+girishjulka@users.noreply.github.com>
2020-09-17 17:08:46 +0530

    Add files via upload

---
+++ UserDashBoard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Base from "../core/Base";
+
+const UserDashBoard = ()=> {
+    return (
+        <Base title="UserDashBoard Page">
+        <h1>This is User DashBoard Page </h1>
+        </Base>
+    );
+};
+export default UserDashBoard;
